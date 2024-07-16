6c81a0e117b7777b7d1e1cc1abafc8eb0cc32f27
Woojin1123 <boom175839@gmail.com>
2024-07-16 10:18:40 +0900

    프로그래머스 두수의 합

---
+++ Programmers/Lv0/prog120803.java
@@ -0,0 +1,18 @@
+package Lv0;
+
+public class prog120803 {
+    public static int solution(int num1, int num2) {
+        int answer = 0;
+
+        answer = num1 - num2;
+
+        return answer;
+    }
+    public static void main(String[] args){
+
+        System.out.println(solution(2,3));
+
+        }
+
+    }
+
