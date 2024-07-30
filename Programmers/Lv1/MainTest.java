71db6c087319fb7b18d36930784a93ff1468083c
Woojin1123 <boom175839@gmail.com>
2024-07-30 09:34:56 +0900

    Programmers Lv1 : 140108

--- Programmers/Lv1/MainTest.java
+++ Programmers/Lv1/MainTest.java
@@ -2,10 +2,9 @@ package Lv1;
 
 public class MainTest {
     public static void main(String[] args){
-        prog42862 s = new prog42862();
-        int[] lost = {2,4,};
-        int[] reserve = {1,3,5};
-        int answer = s.solution(5,lost,reserve);
-        System.out.println(answer);
+        prog140108 s = new prog140108();
+        String c = "aaabbaccccabba";
+        System.out.println(s.solution(c));
+
     }
 }
---
+++ Programmers/Lv1/prog140108.java
@@ -0,0 +1,37 @@
+package Lv1;
+
+public class prog140108 {
+  public int solution(String s) {
+    int answer = 0;
+    int countA = 1, countB = 0;
+    char a = s.charAt(0);
+    if(s.length() == 1){
+      return 1;
+    }
+
+    for(int i = 1; i<s.length();i++){
+      if(s.charAt(i) == a){
+        countA += 1;
+      }else{
+        countB += 1;
+      }
+      if(countA == countB){
+        if(i+1 == s.length()){
+          answer += 1;
+          break;
+        }
+        a = s.charAt(i+1);
+        countA = 0;
+        countB = 0;
+        answer += 1;
+      }else if(i+1 == s.length()){
+        answer += 1;
+        break;
+      }
+
+    }
+
+    return answer;
+  }
+
+}
